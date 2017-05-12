aad2eff7d4613d962f32c1940e2c094e8fbd1e76
caiomartini <caiomartini@hotmail.com>
2017-05-11 23:26:29 -0300

    Update first_run.py

--- first_run.py
+++ first_run.py
@@ -9,30 +9,6 @@ os.makedirs(directory, exist_ok=True)
 directory = "C:/Sonar/issues-report"
 os.makedirs(directory, exist_ok=True)
 
-file = "C:/Sonar/template/template.sonarsource.properties"
-os.makedirs(os.path.dirname(file), exist_ok=True)
-if not os.path.isfile(file):
-    with open(file, "w") as f:
-        f.writelines("sonar.host.url={url}\n"\
-                "sonar.login={login}\n"\
-                "sonar.password={password}\n"\
-                "\n"\
-                "sonar.projectBaseDir={repository}\n"\
-                "sonar.projectKey={key}\n"\
-                "sonar.projectName={sistema}\n"\
-                "sonar.projectVersion=1.0\n"\
-                "\n"\
-                "sonar.sources={path}\n"\
-                "\n"\
-                "sonar.analysis.mode=preview\n"\
-                "\n"\
-                "sonar.exclusions=**/*.js\n"\
-                "\n"\
-                "sonar.issuesReport.html.enable=true\n"\
-                "sonar.issuesReport.html.location=C:/Sonar/issues-report/{sistema}/\n"\
-                "sonar.issuesReport.html.name=issues-report-{sistema}\n"\
-                "sonar.issuesReport.console.enable=true")
-
 print("OK > Directories created.")
 
 print(">> Instal GitPython ...")
