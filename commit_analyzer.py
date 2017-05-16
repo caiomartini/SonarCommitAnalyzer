07664f7fd88d20d593476c071f7d57ceedd3554d
caiomartini <caiomartini@hotmail.com>
2017-05-16 00:14:11 -0300

    Update commit_analyzer.py and utils.py

--- commit_analyzer.py
+++ commit_analyzer.py
@@ -10,6 +10,7 @@ class CommitAnalyzer(object):
     """ Class to analyze commit. """
     def __init__(self):
         config = config_tool.ConfigTool("{replace}/SonarCommitAnalyzer/config.ini")
+
         sonarconfigs = config.configsectionmap("Sonar")
         self.sonar_scanner = sonarconfigs["scanner"]
         self.sonar_server = sonarconfigs["url"]
@@ -17,11 +18,16 @@ class CommitAnalyzer(object):
         self.sonar_password = sonarconfigs["password"]
         self.sonar_folder = sonarconfigs["folder"]
         self.sonar_template = sonarconfigs["template"]
+
         repositoryconfig = config.configsectionmap("Repository")
         self.base_repository = repositoryconfig["repository"]
         self.base_ci = repositoryconfig["ci"]
+
         scanstatus = config.configsectionmap("Status")
         self.scan_status = scanstatus["on"].lower() == "true"
+
+        self.git_repository = git.Repo(self.base_repository)
+        self.git_command = git.Git(self.base_repository)
         self.systems_and_keys = utils.find_systems_and_keys(self.base_ci)
         self.files = []
         self.systems = []
@@ -33,6 +39,7 @@ class CommitAnalyzer(object):
             file = file.a_path
             file_folders = file.split("/")
             folder = self.base_repository + file.replace("/" + file_folders[len(file_folders)-1], "")
+
             for i in range(len(file_folders)-1, 0, -1):
                 folder = folder.replace("/" + file_folders[i], "")
                 for _, _, files in os.walk(folder):
@@ -40,6 +47,7 @@ class CommitAnalyzer(object):
                         if file_system.endswith(".sln"):
                             file_dictionary = {"System": file_system.replace(".sln", ""), "File": file}
                             return file_dictionary
+
         except Exception:
             utils.print_("ERRO > Não foi possível encontrar os sistemas a partir dos arquivos modificados.")
             sys.exit(0)
@@ -51,35 +59,36 @@ class CommitAnalyzer(object):
             system = list({system["System"] for system in self.systems_and_keys if system["Solution_Path"] in file})[0]
             file_dictionary = {"System": system, "File": file}
             return file_dictionary
+
         except Exception:
             utils.print_("ERRO > Não foi possível encontrar os sistemas a partir dos arquivos modificados.")
             sys.exit(0)
 
     def find_modified_files(self):
         """ Function to find modified files. """
-        utils.print_(">> Analisando arquivos no stage ...")
-        
+        utils.print_(">> Analisando arquivos no stage ...")        
+
         try:
-            repository = git.Repo(self.base_repository)
-            modified_files = repository.head.commit.diff()
-            
+            modified_files = self.git_repository.head.commit.diff()
+
             if not modified_files:
                 utils.print_("OK > Nenhum arquivo foi alterado.")
-                sys.exit(0)
-            
+                sys.exit(0)            
+
             for file in modified_files:
                 _, file_extension = os.path.splitext(file.a_path)
                 if file.change_type != "D" and file_extension == ".cs":
                     dictionary = self.find_modified_systems(file)
-                    self.files.append(dictionary)
-            
+                    self.files.append(dictionary)            
+
             if len(self.files) == 0:
                 utils.print_("OK > Nenhum arquivo C# foi alterado.")
                 sys.exit(0)
+
             utils.print_("Arquivos C# alterados:")
             self.systems = {file["System"] for file in self.files}
-            self.systems = sorted(self.systems)
-            
+            self.systems = sorted(self.systems)            
+
             for system in self.systems:
                 index = list(self.systems).index(system)+1
                 utils.print_("{}. {}".format(index, system))
@@ -92,37 +101,42 @@ class CommitAnalyzer(object):
                     #    utils.print_(u"       \u2514 " + file)
                     #else:
                     #    utils.print_(u"       \u251c " + file)
+
         except Exception:
             utils.print_("ERRO > Não foi possível encontrar os arquivos modificados no stage.")
             sys.exit(0)
 
     def remove_configuration_file(self, system):
         """ Function to remove sonar configuration file. """
-        utils.print_(">> Removendo arquivo de configuração ...")
-        
+        utils.print_(">> Removendo arquivo de configuração ...")        
+
         try:
             utils.remove_file(self.sonar_folder + "{}.sonarsource.properties".format(system))
             utils.print_("OK > Arquivo de configuração {}.sonarsource.properties removido com sucesso.".format(system))
+
         except Exception:
             utils.print_("ERRO > Não foi possível remover o arquivo de configuração do sistema {}".format(system))
             sys.exit(0)
 
     def run_sonar(self, system):
         """ Function to run sonar-scanner. """
-        utils.print_(">> Executando SonarQube no sistema {} ...".format(system))
-        
+        utils.print_(">> Executando SonarQube no sistema {} ...".format(system))        
+
         try:
             command = self.sonar_scanner + " -D project.settings={}{}.sonarsource.properties".format(self.sonar_folder, system)
             output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, encoding="utf-8")
+
             if "EXECUTION FAILURE" in output.stdout:
                 utils.print_("ERRO > Não foi possível executar o SonarQube no sistema {}".format(system))
                 sys.exit(0)
+
             if "major" in output.stdout or "critical" in output.stdout:
                 webbrowser.open(self.sonar_folder + "issues-report/{}/issues-report-{}.html".format(system, system), new=2)
                 utils.print_("OK > Relatório disponibilizado no navegador.")
                 self.scanner_error = True
             else:
                 utils.print_("OK > Não foi encontrado nenhum problema no sistema {}".format(system))
+
         except Exception:
             utils.print_("ERRO > Não foi possível executar o SonarQube no sistema {}".format(system))
             sys.exit(0)
@@ -131,8 +145,8 @@ class CommitAnalyzer(object):
 
     def preparing_sonar(self, system):
         """ Function to preparing sonar-scanner execution. """
-        utils.print_(">> Preparando execução do SonarQube no sistema {} ...".format(system))
-        
+        utils.print_(">> Preparando execução do SonarQube no sistema {} ...".format(system))        
+
         lines = []
         replacements = {
             "{url}": self.sonar_server,
@@ -162,19 +176,27 @@ class CommitAnalyzer(object):
         """ Main function to analyze commit. """
         utils.print_("\n")
         utils.print_(">> Processo de análise de código pelo SonarQube iniciado.")
-        
+
+        utils.verify_branch_merging(self.git_command)
         self.find_modified_files()
+
         if self.scan_status:
             utils.verify_sonar_response(self.sonar_server)
+
             for system in self.systems:
                 self.preparing_sonar(system)
                 self.run_sonar(system)
+
             utils.remove_folder("{}.scannerwork".format(self.base_repository))
             utils.print_(">> Análise de qualidade de código pelo SonarQube finalizada.")
+
             if self.scanner_error:
                 utils.print_("WARN > Foram encontrados problemas críticos de qualidade, verifique o relatório no navegador.")
                 utils.print_("WARN > Commit recusado.")
                 sys.exit(1)
+
             utils.print_("OK > Nenhum problema foi encontrado. Commit liberado.")
         else:
             utils.print_(">> Análise de qualidade de código pelo SonarQube desativada.")
+
+        utils.print_("\n")
--- utils.py
+++ utils.py
@@ -4,19 +4,21 @@ import os
 import shutil
 
 def print_(text):
-    """ Função para exibir texto e forçar atualização do console. """
+    """ Function to print and flush console. """
     print(text)
     sys.stdout.flush()
 
 def verify_sonar_response(url):
     """ Function to verify SonarQube is running on server. """
     print_(">> Verificando se SonarQube está em execução no servidor ...")
+
     try:
         url = url.replace("http://", "")
         sonarhttp = http.client.HTTPConnection(url)
         sonarhttp.request("HEAD", "/")
         response = sonarhttp.getresponse()
         print_("OK > SonarQube está em execução.")
+
     except Exception:
         print_("ERROR > SonarQube não está em execução. Commit liberado.")
         sys.exit(0)
@@ -31,29 +33,43 @@ def remove_folder(folder):
     if os.path.isdir(folder):
         shutil.rmtree(folder)
 
+def verify_branch_merging(git_command):
+    """ Function to verify branch is merging. """
+    branch_merging = git_command.execute("git status")
+
+    if "All conflicts fixed but you are still merging." in branch_merging:
+        print_(">> Commit de MERGE. SonarQube não será executado.")
+        sys.exit(0)
+
 def find_systems_and_keys(repository):
     """ Created for Mitsui. Function find systems and keys in ps1 file. """
     file = repository + "Configuracoes.ps1"
+
     replacements = {
         "),\n": "",
         ")\n": ""
     }
+
     systems_keys = []
+
     with open(file, encoding="utf8") as f:
         for line in f:
             if "new-DotNetSolution -ID" in line:
                 find_line = list(line.replace("'", "").split(" "))
+
                 solution = find_line[find_line.index("-Solution") + 1].split("\\")
                 key = find_line[find_line.index("-ID") + 1]
                 solution_path = find_line[find_line.index("-Solution") + 1].replace(solution[len(solution)-1], "")
                 solution = list({index for index in find_line[find_line.index("-Solution") + 1].split("\\") if ".sln" in index})[0].replace(".sln", "")
                 version = find_line[find_line.index("-Version") + 1]
                 language = find_line[find_line.index("-Language") + 1]
+
                 for src, target in replacements.items():
                     key = key.replace(src, target)
                     solution = solution.replace(src, target)
                     version = version.replace(src, target)
                     language = language.replace(src, target)
+
                 system = {
                     "System": solution,
                     "Key": key,
@@ -61,5 +77,7 @@ def find_systems_and_keys(repository):
                     "Version": version,
                     "Language": language
                 }
+
                 systems_keys.append(system)
+
     return systems_keys
